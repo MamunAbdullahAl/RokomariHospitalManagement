1c49b7022c826c5f6026052524c1dd1f113d234b
MamunAbdullahAl <mamunabdullah6055@gmail.com>
2017-02-28 11:44:03 +0600

    initial commit

---
+++ src/main/java/com/RokomariHospitalManagement/RokomariHospitalManagementApplication.java
@@ -0,0 +1,12 @@
+package com.RokomariHospitalManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RokomariHospitalManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RokomariHospitalManagementApplication.class, args);
+	}
+}
---
+++ src/test/java/com/RokomariHospitalManagement/RokomariHospitalManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.RokomariHospitalManagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RokomariHospitalManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
